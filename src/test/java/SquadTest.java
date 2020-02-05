3381c828e828722e4355503f169fd27aaa511ce5
Peter-cloud-web <petergichia164@gmail.com>
2020-02-05 22:18:58 +0300

    Squad test passes

--- src/test/java/SquadTest.java
+++ src/test/java/SquadTest.java
@@ -1,5 +1,35 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SquadTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    private Squad setupSquad() {
+        return new Squad("legacy","bad guys");
+    }
+    @Test
+    public void NewSquadObjectGetsCorrectlyCreated_true() throws Exception {
+        Squad squad = setupSquad();
+        assertTrue( squad instanceof Squad);
+    }
+    @Test
+    public void getSquadName_String(){
+        Squad squad = setupSquad();
+        assertEquals("legacy",squad.getSquadName());
+    }
+    @Test
+    public void getSquadCause_String(){
+        Squad squad = setupSquad();
+        assertEquals("bad guys", squad.getCause());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
 }
